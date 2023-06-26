50e2b6a42371b3a9bfe76991de08a926f50f3ea9
swatibadiger <134912149+swatibadiger@users.noreply.github.com>
2023-06-26 05:08:03 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ streamlit.text('Avocado Toast')
 streamlit.header('Build Your Own Fruit Smoothie')
 
 import pandas
-my_fruit_list = pands.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
 
 
