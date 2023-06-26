e2f34ac58549313edc227f82ffd507314c0cbd7b
swatibadiger <134912149+swatibadiger@users.noreply.github.com>
2023-06-26 04:05:04 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,9 @@
 import streamlit
 streamlit.title('My Perents New Healthy Diner')
+
+import streamlit
+streamlit.title('My Perents New Healthy Diner')
+streamilt.header('Breakfast Menu')
+streamilt.text(Omega 3 Blueberry Oatmeal')
+streamilt.text('Kale, Spinach & Rocket Smoothie')
+streamilt.text('Hard-Boiled Free-Range Egg')
