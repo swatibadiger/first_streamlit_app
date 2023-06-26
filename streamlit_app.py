1e194d3fb1ce6add9336bfa40a191389ab2bf05b
swatibadiger <134912149+swatibadiger@users.noreply.github.com>
2023-06-26 04:30:32 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,3 +19,5 @@ streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.text('Avocado Toast')
+
+streamlit.header('Build Your Own Fruit Smoothie')
