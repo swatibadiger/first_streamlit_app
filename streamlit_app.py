8d97fa6a1c95d81736d25e27cb598b6f2415cc6d
swatibadiger <134912149+swatibadiger@users.noreply.github.com>
2023-06-26 04:57:37 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,3 +21,10 @@ streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.text('Avocado Toast')
 
 streamlit.header('Build Your Own Fruit Smoothie')
+
+import pandas
+my_fruit_list = pands.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
+
+
+
